b0ade5d35403ca20bec4647f419f08762bfc1421
Tuna Gündüz <tnhn_fb_007@hotmail.com>
2022-02-15 18:57:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# php-basic
+Basic boilerplate for your next php project.
+You dont need to setup your file structure and bootstrap every time you start a new project.
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Boilerplate</title>
+    <?php
+        //!Includes
+        include "./src/components/bootstrap.php";
+    ?> 
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-4 text-center">
+            <div class="col-2">
+                
+            </div>
+            <div class="col">
+                <img src="https://www.php.net/images/logos/php-logo-bigger.png" class="img-responsive mt-3" width="250" height="" alt="php logo">
+                <p class="mt-4 display-4">this repository is for new started projects.</p>
+                <hr style="border:solid 2px black; opacity:1" />
+                <p class=" display-4">feel free to use, share and modify</p>
+                <hr style="border:solid 2px black; opacity:1" />
+                <div class="github-card" data-github="SirMacr0/php-basic" data-width="400" data-height="150" data-theme="default"></div>
+            </div>
+            <div class="col-2">
+
+            </div>
+        </div>
+    </div>
+    <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+</body>
+</html>
